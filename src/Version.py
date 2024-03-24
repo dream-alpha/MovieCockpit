5b23d94b5c3b11a5c4692a3e3b44f470ab044fe4
dream-alpha <swmaniacster@gmail.com>
2024-03-24 13:45:31 +0100

    18.1.0

--- src/Version.py
+++ src/Version.py
@@ -21,6 +21,6 @@
 
 PLUGIN = "MovieCockpit"
 ID = "MVC"
-VERSION = "18.0.9"
+VERSION = "18.1.0"
 COPYRIGHT = "2018-2024 by dream-alpha"
 LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version."
